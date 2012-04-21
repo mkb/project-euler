88cf3ca604c00fa962c8b070d2693a6840e9ee46
Michael Brodhead <mkb@engineyard.com>
2012-04-20 21:17:40 -0700

    #3 done

--- problem_03.rb
+++ problem_03.rb
@@ -1,7 +1,42 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 
 # The prime factors of 13195 are 5, 7, 13 and 29.
-# 
+#
 # What is the largest prime factor of the number 600851475143 ?
 
+require 'mathn'
+
+STARTING_DIVIDEND = 600851475143
+
+class Monkey
+  def initialize
+    @primer = Prime.new
+    @factors = []
+
+    @dividend = STARTING_DIVIDEND
+  end
+
+  def done?
+    return false if @factors.empty?
+    return @factors.last > @dividend
+  end
+
+  def go
+    until done? do
+      a_prime = @primer.next
+      if @dividend % a_prime == 0
+        @factors << a_prime
+        @dividend = @dividend / a_prime
+        puts "#{a_prime} : #{@dividend}"
+      end
+    end
+    puts "Factors:  #{@factors.join(', ')}"
+    puts "Dividend:  #{@dividend}"
+  end
+end
+
+# require 'rubinius/debugger'
+# Rubinius::Debugger.start
+m = Monkey.new
+m.go
 
---
+++ problem_04.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# A palindromic number reads the same both ways. The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+class Palindromer
+end
+
+puts ENV['TM_RUBY']
+puts $0
+
+exit
+require 'minitest/autorun'
+
+describe Palindromer do
+
+end
